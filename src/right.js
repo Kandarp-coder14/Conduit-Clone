4cbc3df043ece1aba8678ba4d65b9b730fb8e450
Kandarp-coder14 <klalit1410@gmail.com>
2019-12-19 22:57:35 +0530

    conduit clone frontend donr

---
+++ src/right.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+function Right() {
+  return (
+    <div className="right">
+      <div className="right-sub"></div>
+      <div className="box-1">
+        <div className="makers_container">
+          <div className="makers">
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/2107045/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/826276/original?auto=…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/1400598/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/166755/original?auto=…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src=" https://ph-avatars.imgix.net/1384140/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src=" https://ph-avatars.imgix.net/2097425/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/1528972/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/2107045/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/1528972/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/1381106/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/2107045/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+            <img
+              className="cir"
+              src="https://ph-avatars.imgix.net/2191329/original?auto…ompress&codec=mozjpeg&cs=strip&w=38&h=38&fit=crop"
+              alt="i"
+            />
+          </div>
+          <hr></hr>
+          <h2 className="join">JOIN THEM ON MAKERS</h2>
+        </div>
+
+        <br />
+        <h3>Product Hunt Radio</h3>
+        <br />
+        <div className="future">
+          <h5>The future of seed stage VC with Tige Savage</h5>
+        </div>
+        <div>
+          <h3 className="news">Newsletter</h3>
+          <div className="fff">
+            <img className="final" src="gif.gif" alt="img" />
+            <div className="place">
+              <p>Get the best new products in your inbox, every day 👇</p>
+              <input className="email" placeholder="Your email" />
+              <br />
+              <button className="btn">SUBSCRIBE</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Right;
